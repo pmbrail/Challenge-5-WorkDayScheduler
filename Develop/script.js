bc16f184e3f85db38885a25e227b36d9ed0e9670
PMB <pmbrailsford223@gmail.com>
2023-07-18 20:01:48 -0700

    worked on js and readme

--- Develop/script.js
+++ Develop/script.js
@@ -1,13 +1,22 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-var past;
-var present;
-var future;
+var confirmPast;
+var confirmPresent;
+var confirmFuture;
+var timeAM = ["1AM", "2AM", "3AM", "4AM", "5AM", "6AM", "7AM", "8AM", "9AM", "10AM", "11AM", "12AM"];
+var timePM = ["1PM", "2PM", "3PM", "4PM", "5PM", "6PM", "7PM", "8PM", "9PM", "10PM", "11PM", "12PM"];
 var getdateandtime = 
 
 $(function () {
-  var past = display()
+  saveBtn.addEventListener("click", hour-x);
+
+  function confirmPast () {
+    var confirmPast = display ("gray")
+    if(timeAM < 1PM) {
+      timAM = display ("gray")
+    }
+  }
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Challenge-5-WorkDayScheduler
+WorkDay Scheduler
+
+## Description
+
+This challenge focused on building a work day scheduler based on dynamically updated HTML and CSS powered by Javascript code. 
+
+## Credits
+
+Followed the ASU Bootcamp tutorials shown in the ASU-VIRT-FSF-PT-06-2023-U-LOLC. 
+
+## License
+
+No license
+
+--- 
